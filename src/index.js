9a2d29270f2787b58d8f34c288d2701d2d0d180f
devWaz <fawazodessy@gmail.com>
2022-08-28 20:31:19 +0100

    Everything working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { QuizProvider } from "./contexts/quiz";
+import Quiz from "./components/quiz";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QuizProvider>
-    <App />
+    <Quiz/>
     </QuizProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 
